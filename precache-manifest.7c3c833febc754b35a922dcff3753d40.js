e758f5a49fc6177a2660e29706948b937de4779c
venturads <albert@venturads.com>
2020-08-07 23:40:58 -0700

    Updates

---
+++ precache-manifest.7c3c833febc754b35a922dcff3753d40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54e8fecb28b6734e772c1cea66a676f",
+    "url": "/venturads/react-autho.git/index.html"
+  },
+  {
+    "revision": "d8b53eaba592d65771fc",
+    "url": "/venturads/react-autho.git/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c19906d3ad7ac06159bc",
+    "url": "/venturads/react-autho.git/static/js/2.a37161b6.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/venturads/react-autho.git/static/js/2.a37161b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b53eaba592d65771fc",
+    "url": "/venturads/react-autho.git/static/js/main.45d9ff6e.chunk.js"
+  },
+  {
+    "revision": "59a8465dae076bf5e69a",
+    "url": "/venturads/react-autho.git/static/js/runtime-main.4f67cacf.js"
+  }
+]);
