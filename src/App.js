b8a2eb7b62e7dcbd135ee05d464456f8c941edf4
venturads <albert@venturads.com>
2020-08-07 21:36:15 -0700

    create user profile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <LoginButton />
+    <div>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </div>
   );
 }
 
