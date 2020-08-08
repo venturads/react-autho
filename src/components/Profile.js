1c946e0736b4b86d1dedc0f416e6202b5f41771d
venturads <albert@venturads.com>
2020-08-07 21:44:44 -0700

    add json pretty and user isAuthen to see image and data

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import JSONPretty from 'react-json-pretty';
 
 const Profile = () => {
-    const { user } = useAuth0();
+    const { user, isAuthenticated } = useAuth0();
     return (
-        <div>
-            {JSON.stringify(user, null, 2)}
-        </div>
+        isAuthenticated && (
+            <div>
+                <img src={user.picture} alt={user.name} />
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+                <JSONPretty data={user} />
+                {/*JSON.stringify(user, null, 2)*/}
+            </div>
+        )
     )
 }
 
